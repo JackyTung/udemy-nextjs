8b00b4b0cf52ab8022c043006a7004beb557949e
Jacky Tung <jacky.tung@langlive.com>
2021-07-05 00:12:44 +0800

    course-4: add icon

--- components/events/EventItem.jsx
+++ components/events/EventItem.jsx
@@ -1,4 +1,7 @@
-import Link from 'next/link'
+import AddressIcon from '../icons/address-icon'
+import DateIcon from '../icons/date-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
+import Button from '../ui/Button'
 
 import classes from './event-item.module.css'
 
@@ -18,14 +21,21 @@ const EventItem = ({ title, image, date, location, id }) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore event</Link>
+          <Button link={exploreLink}>
+            <span>Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
