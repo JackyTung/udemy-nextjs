e74a213832e49754d3fc84e9e436211c71c952a6
Jacky Tung <jacky.tung@langlive.com>
2021-07-04 20:10:33 +0800

    course4: init EventList and EventItem

---
+++ components/events/EventList.jsx
@@ -0,0 +1,21 @@
+import EventItem from './EventItem'
+
+const EventList = ({items = []}) => {
+    return (
+        <ul>
+            {items.map(event => (
+                <EventItem 
+                    key={event.id}
+                    id={event.id}
+                    title={event.title}
+                    image={event.image}
+                    date={event.date}
+                    location={event.location}
+                />
+            )
+        )}
+        </ul>
+    )
+}
+
+export default EventList
