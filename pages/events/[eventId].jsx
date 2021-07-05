d8b2ab380ff43918e3d4bd758c0c57e06fc445f6
Jacky Tung <jacky.tung@langlive.com>
2021-07-05 09:03:11 +0800

    course-4: dynamic routing

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,7 +1,28 @@
-export default function EventDetailPage() {
-    return (
-      <div>
-        <h1>Event detail</h1>
-      </div>
-    )
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
+
+import EventContent from '../../components/event-detail/event-content'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventSummary from '../../components/event-detail/event-summary'
+import { getEventById } from '../../dummy-data'
+
+const EventDetailPage = () => {
+  const router = useRouter()
+  const { eventId } = router.query
+  const event = getEventById(eventId)
+  if (!event) {
+    return <p>No event from </p>
   }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  )
+}
+
+export default EventDetailPage
